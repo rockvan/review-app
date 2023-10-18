aec2fe6d2a918da0ea1d1bd17654208b6c60e227
rockvan <dev.rockvan@gmail.com>
2023-10-18 22:23:28 +0800

    added cheer

---
+++ src/lib/utils/cheer.ts
@@ -0,0 +1,12 @@
+export const cheers = [
+	'Go mom, go!',
+	"You're the best mom ever!",
+	'Mom, you rock!',
+	"You're doing an amazing job, mom!",
+	'Hip hip hooray for mom!',
+	"Mom, you're a superhero!",
+	'You make the world a better place, mom!',
+	"Mom, you're the glue that holds our family together!",
+	'Cheers to the most incredible mom!',
+	'We love you, mom!'
+];
